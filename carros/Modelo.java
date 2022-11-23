1ec5310ae702ca995d84f731f76f44feca0f4f19
fabiop87 <100612310+fabiop87@users.noreply.github.com>
2022-11-22 21:45:40 -0300

    Add files via upload

---
+++ carros/Modelo.java
@@ -0,0 +1,39 @@
+package veiculos;
+
+public class Modelo {
+    
+    
+    public String NomeModelo;
+    public int PrecoModelo;
+
+    public Modelo(String NomeModelo, int PrecoModelo) {
+        this.NomeModelo = NomeModelo;
+        this.PrecoModelo = PrecoModelo;
+    }
+
+    
+    
+    public String getNomeModelo() {
+        return NomeModelo;
+    }
+
+    public void setNomeModelo(String NomeModelo) {
+        this.NomeModelo = NomeModelo;
+    }
+
+    public int getPrecoModelo() {
+        return PrecoModelo;
+    }
+
+    public void setPrecoModelo(int PrecoModelo) {
+        this.PrecoModelo = PrecoModelo;
+    }
+    
+
+    
+    
+ 
+    
+    
+    
+}
